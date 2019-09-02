8f3472fade94c25abe5650248126bfc206dad0d7
Alexander Ivanov <33380175+xanderhub@users.noreply.github.com>
2019-09-02 14:15:44 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# business-scenario-simulator
+ App for simulating business scenarios based on FSM (Final State Machine) principal
---
+++ src/main/java/com/xanderhub/businessscenariosimulator/BusinessScenarioSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.xanderhub.businessscenariosimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessScenarioSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessScenarioSimulatorApplication.class, args);
+	}
+
+}
